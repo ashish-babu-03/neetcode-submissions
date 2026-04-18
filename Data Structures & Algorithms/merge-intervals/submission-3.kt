9e45081b35ad30afc0badb8a567d64eed86812ff
Ashish Babu <ashish.babu.sde@gmail.com>
2026-04-18 12:55:13 +0530

    Add: merge-intervals - submission-3

---
+++ Data Structures & Algorithms/merge-intervals/submission-3.kt
@@ -0,0 +1,21 @@
+class Solution {
+    fun merge(intervals: Array<IntArray>): Array<IntArray> {
+        intervals.sortBy { it[0] }
+        val result = mutableListOf<IntArray>()
+        for (i in intervals) {
+            if (result.isEmpty()) result.add(i)
+            else {
+                val prev = result.last()
+                val next = i
+                if (prev[1] >= next[0]) {
+                    result.remove(prev)
+                    val first = minOf(prev[0], next[0])
+                    val last = maxOf(prev[1], next[1])
+                    result.add(intArrayOf(first, last))
+                }
+                else result.add(i) 
+            }
+        }
+        return result.toTypedArray()
+    }
+}
