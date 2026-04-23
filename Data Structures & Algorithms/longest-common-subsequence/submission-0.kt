c3abaec125b1970c516b61befaf3964f323c49f6
Ashish Babu <ashish.babu.sde@gmail.com>
2026-04-23 16:17:36 +0530

    Add: longest-common-subsequence - submission-0

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-0.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun longestCommonSubsequence(text1: String, text2: String): Int {
+        val dp = Array (text1.length + 1) { IntArray(text2.length + 1) {0} }
+        for (i in 1..text1.length) {
+            for (j in 1..text2.length) {
+                if (text1[i-1] == text2[j-1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                } else {
+                    dp[i][j] = maxOf(dp[i][j-1], dp[i-1][j])
+                }
+            }
+        }
+
+        return dp[text1.length][text2.length]
+    }
+}
