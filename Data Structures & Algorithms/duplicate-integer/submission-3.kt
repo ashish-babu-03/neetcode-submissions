3b2411e811c7b473283a1cb71f51d7564350d99d
Ashish Babu <ashish.babu.sde@gmail.com>
2026-05-14 22:06:32 +0530

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.kt
@@ -0,0 +1,8 @@
+class Solution {
+    fun hasDuplicate(nums: IntArray): Boolean {
+        val seen = HashSet<Int>()
+        for (num in nums) if (!seen.add(num)) return true
+
+        return false
+    }
+}
