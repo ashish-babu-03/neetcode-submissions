cc1cc5e2e65efe782b1b5b1f9d482457d09e9d89
Ashish Babu <ashish.babu.sde@gmail.com>
2026-05-20 14:45:21 +0530

    Add: clone-graph - submission-2

---
+++ Data Structures & Algorithms/clone-graph/submission-2.kt
@@ -0,0 +1,20 @@
+/*
+Definition for a Node.
+class Node(var `val`: Int) {
+    var neighbors: ArrayList<Node?> = ArrayList()
+}
+*/
+
+class Solution {
+    val cloneMap = mutableMapOf<Node, Node>()
+    fun cloneGraph(node: Node?): Node? {
+        if (node == null) return null
+        val temp = Node(node.`val`)
+        cloneMap[node] = temp
+        for (neighbor in node.neighbors) {
+            if (!cloneMap.containsKey(neighbor)) cloneGraph(neighbor)!!
+            temp.neighbors.add(cloneMap[neighbor])
+        }
+        return temp
+    }
+}
